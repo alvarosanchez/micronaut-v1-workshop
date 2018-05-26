e87d07eeeedd399e9d47756cba1904248aaf0db8
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-05-26 15:28:38 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git checkout -b gh-pages
+git merge master
+
+for adoc in `find . -name "*.adoc"`
+do
+	cd `dirname $adoc`
+	asciidoctor `basename $adoc`
+	cd $OLDPWD
+done
+
+cp README.html index.html 
+git commit -a -m "Updated files"
+git push origin gh-pages
+git checkout master
