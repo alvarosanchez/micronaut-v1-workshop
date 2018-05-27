3d9bd1e87105d974bfa20e0db92f29353b64468f
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-05-27 19:50:22 +0200

    Updated docs

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@ git commit -a -m "Updated docs"
 set -e
 git push origin master
 
-asciidoctor -r asciidoctor-diagram -a source-highlighter=pygments -a generate-diagrams README.adoc
+#Generate diagrams
+asciidoctor -r asciidoctor-diagram -a generate-diagrams README.adoc
+
+#Generate HTML
+asciidoctor -a source-highlighter=pygments README.adoc
+
 git checkout gh-pages
 mv README.html index.html
 mv *.png images/
