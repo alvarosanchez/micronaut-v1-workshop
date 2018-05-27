a96c48178cac14353fcf8948552b023d8444739f
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-05-27 12:18:53 +0200

    Updated publishing script

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
+set -e
 
+asciidoctor README.adoc
 git checkout gh-pages
-git merge master
-
-for adoc in `find . -name "*.adoc"`
-do
-	cd `dirname $adoc`
-	asciidoctor `basename $adoc`
-	cd $OLDPWD
-done
-
-cp README.html index.html 
-git commit -a -m "Updated files"
+mv README.html index.html 
+git commit -a -m "Updated docs"
 git push origin gh-pages
 git checkout master
+rm index.html
