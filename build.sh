dc22d6d2f25aa1852f7cb21efecfc02d02b12c99
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-05-27 12:26:00 +0200

    Updated docs

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 git commit -a -m "Updated docs"
+git push origin master
+
 asciidoctor README.adoc
 git checkout gh-pages
 mv README.html index.html 
 git commit -a -m "Updated docs"
 git push origin gh-pages
 git checkout master
-rm index.html
