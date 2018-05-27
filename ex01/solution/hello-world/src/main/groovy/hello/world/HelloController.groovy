1ccedc1204f4ac26162d0aaf8467a189754960c6
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-05-27 17:45:03 +0200

    First exercise

---
+++ ex01/solution/hello-world/src/main/groovy/hello/world/HelloController.groovy
@@ -0,0 +1,15 @@
+package hello.world
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.HttpStatus
+
+
+@Controller("/hello")
+class HelloController {
+
+    @Get("/")
+    String index() {
+        return "Hello Micronauts!"
+    }
+}
---
+++ ex01/solution/hello-world/src/test/groovy/hello/world/HelloControllerSpec.groovy
@@ -0,0 +1,25 @@
+package hello.world;
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class HelloControllerSpec extends Specification {
+
+    @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+    @Shared @AutoCleanup RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+
+
+    void "test index"() {
+        given:
+        HttpResponse response = client.toBlocking().exchange("/hello")
+
+        expect:
+        response.status == HttpStatus.OK
+    }
+}
