105303ef1ca1e7efb0d073e4d00eaeb8ea04aec6
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-05-27 13:08:49 +0200

    Updated docs

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 git commit -a -m "Updated docs"
 git push origin master
 
-asciidoctor README.adoc
+asciidoctor -a source-highlighter=pygments README.adoc
 git checkout gh-pages
 mv README.html index.html 
 git commit -a -m "Updated docs"
