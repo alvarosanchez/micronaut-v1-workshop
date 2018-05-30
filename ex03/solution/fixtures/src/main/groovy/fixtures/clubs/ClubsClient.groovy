5353b989e2be5b4db903a722c672a5ff7766cd2c
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-05-30 11:31:52 +0200

    Updated docs

--- ex03/solution/fixtures/src/main/groovy/fixtures/clubs/ClubsClient.groovy
+++ ex03/solution/fixtures/src/main/groovy/fixtures/clubs/ClubsClient.groovy
@@ -1,10 +1,8 @@
 package fixtures.clubs
 
 import io.micronaut.http.client.Client
-import io.micronaut.retry.annotation.Recoverable
 
 //tag::class[]
 @Client("clubs")    // <1>
-@Recoverable        // <2>
 interface ClubsClient extends ClubsApi {}
 //end::class[]
