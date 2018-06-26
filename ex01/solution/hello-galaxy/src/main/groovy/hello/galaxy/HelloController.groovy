85e5420cd99ea71d5120f4e576dabf7164aefd28
sdelamo <sergio.delamo@softamo.com>
2018-06-26 11:55:45 +0200

    Adds @CompileStatic

--- ex01/solution/hello-galaxy/src/main/groovy/hello/galaxy/HelloController.groovy
+++ ex01/solution/hello-galaxy/src/main/groovy/hello/galaxy/HelloController.groovy
@@ -1,9 +1,10 @@
 package hello.galaxy
 
+import groovy.transform.CompileStatic
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 
-
+@CompileStatic
 @Controller("/hello")
 class HelloController {
 
