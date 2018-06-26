2bc11b4398a3dd93b30bed6e5f58501681070a17
sdelamo <sergio.delamo@softamo.com>
2018-06-26 11:54:46 +0200

    Annotate with @CompileStatic

--- ex02/solution/clubs/src/main/groovy/clubs/client/ClubsClient.groovy
+++ ex02/solution/clubs/src/main/groovy/clubs/client/ClubsClient.groovy
@@ -1,7 +1,9 @@
 package clubs.client
 
 import clubs.api.ClubsApi
+import groovy.transform.CompileStatic
 import io.micronaut.http.client.Client
 
+@CompileStatic
 @Client("/")
 interface ClubsClient extends ClubsApi {}
--- ex02/solution/clubs/src/main/groovy/clubs/domain/Club.groovy
+++ ex02/solution/clubs/src/main/groovy/clubs/domain/Club.groovy
@@ -1,8 +1,10 @@
 package clubs.domain
 
 import grails.gorm.annotation.Entity
+import groovy.transform.CompileStatic
 import org.grails.datastore.gorm.GormEntity
 
+@CompileStatic
 @Entity
 class Club implements GormEntity<Club> {
 
