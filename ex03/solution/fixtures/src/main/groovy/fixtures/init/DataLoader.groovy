3e542dd30bed279d6ac83066714adb52a5e6f028
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-07-25 11:14:03 -0500

    Updated docs

---
+++ ex03/solution/fixtures/src/main/groovy/fixtures/init/DataLoader.groovy
@@ -0,0 +1,32 @@
+package fixtures.init
+
+import fixtures.domain.Fixture
+import fixtures.service.FixtureService
+import groovy.util.logging.Slf4j
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.env.Environment
+import io.micronaut.context.event.ApplicationEventListener
+import io.micronaut.runtime.server.event.ServerStartupEvent
+
+import javax.inject.Singleton
+
+@Slf4j
+@Singleton
+@Requires(notEnv = Environment.TEST)
+class DataLoader implements ApplicationEventListener<ServerStartupEvent> {
+
+    final FixtureService fixtureService
+
+    DataLoader(FixtureService fixtureService) {
+        this.fixtureService = fixtureService
+    }
+
+    @Override
+    void onApplicationEvent(ServerStartupEvent event) { // <4>
+        if (!fixtureService.count()) {
+            log.debug "Loading sample data"
+            Fixture fixture = new Fixture(homeClubId: 1, homeScore: 5, awayClubId: 2, awayScore: 0, date: new Date())
+            fixtureService.save(fixture)
+        }
+    }
+}
